44093a5109cad9660d17f792d3ed727c7f5b98ea
CHAMUD12 <chamudgamage@gmail.com>
2024-05-23 09:34:18 +0530

    scroll section show in mobile nav

--- js/main.js
+++ js/main.js
@@ -56,3 +56,24 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  /* Scroll section show in mobile nav */
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+  
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+  
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+      sectionsClass.classList.add('active-link')
+    }else{
+      sectionsClass.classList.remove('active-link')
+    }                                                    
+  })
+}
+window.addEventListener('scroll', scrollActive)
